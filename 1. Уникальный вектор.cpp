e468897add6be57ddf82bddb3f3fa0830d3733be
vix <churink9@gmail.com>
2024-07-03 17:54:23 +0300

    first commit

---
+++ 1. Уникальный вектор.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> removeDuplic(std::vector<int> vec)
+{
+    std::sort(vec.begin(), vec.end());
+
+    auto it = std::unique(vec.begin(), vec.end());
+
+    vec.erase(it, vec.end());
+
+    return vec;
+}
+
+int main()
+{
+    std::vector<int> input = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::vector<int> result = removeDuplic(input);
+
+    auto inputIt = input.begin();
+    std::cout << "[IN]:";
+    while (inputIt != input.end())
+    {
+        std::cout << *inputIt << " ";
+        inputIt++;
+    }
+    std::cout << std::endl;
+
+    auto resultIt = result.begin();
+    std::cout << "[OUT]:";
+    while (resultIt != result.end())
+    {
+        std::cout << *resultIt << " ";
+        resultIt++;
+    }
+
+    return 0;
+}
